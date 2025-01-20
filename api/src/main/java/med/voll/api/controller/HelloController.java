0147ae0d785f014101e25222edb09f6f876b65cb
luan-arend <luan00luchie@hotmail.com>
2025-01-20 20:11:00 -0300

    Incluindo metodos HTTP para buscar,adicionar, atualizar e remover medicos

--- api/src/main/java/med/voll/api/controller/HelloController.java
+++ api/src/main/java/med/voll/api/controller/HelloController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
 
     @GetMapping
-    public String olaMundo(){
-        return "Olá Mundo teste!";
+    public String olaMundo() {
+        return "Hello World Spring!";
     }
 }
